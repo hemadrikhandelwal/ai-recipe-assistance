2e75ffaabc3e4d80ea23175147070289a247fd09
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-07-10 18:52:28 +0530

    feat: protected routes

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+
+function ProtectedRoute() {
+  const { isAuthenticated } = useContext(AuthContext);
+  console.log(isAuthenticated);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/" replace />;
+  }
+
+  return <Outlet />;
+}
+
+export default ProtectedRoute;
