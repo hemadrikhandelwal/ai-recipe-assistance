86cc8c344b8382d0c22a11c81cd5b52dad3d84ee
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-07-11 17:24:53 +0530

    new folder structure

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,8 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+
 
 const Home = () => {
 
-  const { logout } = useContext(AuthContext);
-  const navigate = useNavigate();
 
-  const handleLogout = () => {
-    logout();
-    navigate("/", { replace: true });
-  };
 
 
 };
