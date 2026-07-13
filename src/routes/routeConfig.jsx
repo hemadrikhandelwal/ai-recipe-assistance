d806e63b47f9bfe6d82f5e770bab931b00736f8a
hemadrikhandelwal <hemadrikhandelwal@gmail.com>
2026-07-13 09:05:13 +0530

    remove unnessary routes

--- src/routes/routeConfig.jsx
+++ src/routes/routeConfig.jsx
@@ -1,9 +1,5 @@
 import HomePage from "../pages/HomePage";
 import Search from "../pages/Search";
-import Categories from "../pages/Categories";
-import Areas from "../pages/Areas";
-import Favourites from "../pages/Favourites";
-import MealPlanner from "../pages/MealPlanner";
 import RecipeDetails from "../pages/RecipeDetails";
 
 export const appRoutes = [
@@ -15,22 +11,7 @@ export const appRoutes = [
     path: "/search",
     element: <Search />,
   },
-  {
-    path: "/categories",
-    element: <Categories />,
-  },
-  {
-    path: "/areas",
-    element: <Areas />,
-  },
-  {
-    path: "/favorites",
-    element: <Favourites />,
-  },
-  {
-    path: "/meal-planner",
-    element: <MealPlanner />,
-  },
+
   {
     path: "/recipe/:id",
     element: <RecipeDetails />,
